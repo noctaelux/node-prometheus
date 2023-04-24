e03500d1ae9ff100390eb8981ca1bf410e610b22
Oscar Calzada <noctaelux@gmail.com>
2023-04-24 02:14:27 -0600

    primer commit poc node prometheus

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const express = require('express');
 const prom = require('prom-client');
 
+const collectDefaultMetrics = prom.collectDefaultMetrics;
+const app = express();
 const port = 8080;
+
+app.get('/', function (req, res) {
+    res.send('Welcome page');
+});
+
+app.get('/hola-mundo', function (req, res) {
+    res.send('Hola mundo');
+});
+
+
+collectDefaultMetrics();
+app.get('/metrics', async( req , res ) => {
+    res.set('Content-Type', prom.register.contentType);
+    return res.send(await prom.register.metrics());
+});
+
+app.get('*', function (req, res) {
+    res.send('404 | Page not found');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
